3faee6c08d662e53519f2341714f084aabf0ec37
dalifyyy <100699751+Dalifried@users.noreply.github.com>
2023-12-20 23:26:31 +0800

    Update process.php

--- process.php
+++ process.php
@@ -5,8 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $message = $_POST["message"];
 
-    $client = new MongoDB\Client("mongodb://localhost:27017");
-    $database = $client->selectDatabase("msgftstars");
+    $client = new MongoDB\Client("mongodb://d4lified:LjfkBRVnMvQyRhq3@host:port/mftstars
+");
+    $database = $client->selectDatabase("mftstars");
     $collection = $database->selectCollection("messages");
 
     $document = [
